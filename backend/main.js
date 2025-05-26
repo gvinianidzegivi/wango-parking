694ef78e724c89b0d760bb745c334e88289454c0
Peteris Zemitis <peteris.zemitis@globalmeridianservices.com>
2025-05-26 09:20:17 +0100

    setup cors

--- backend/main.js
+++ backend/main.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const cors = require("cors");
 
 dotenv.config();
 connectDB();
 
 const app = express();
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:5174",
+    credentials: true,
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
